8f8c1ec51ddbbedac0004eb9f6aa3c1e55f127be
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-12-06 00:28:44 -0300

    Componente icon

--- src/screens/HomeScreen/patterns/Feed/Feed.tsx
+++ src/screens/HomeScreen/patterns/Feed/Feed.tsx
@@ -1,4 +1,5 @@
 import Box from "@src/components/Box/Box";
+import Icon from "@src/components/Icon/Icon";
 import Text from "@src/components/Text/Text";
 import React from "react";
 
@@ -17,6 +18,10 @@ export default function Feed({ children }: FeedProps) {
 Feed.Header = () => {
   return (
     <Box>
+      <Icon name="youtube" />
+      <Icon name="twitter" />
+      <Icon name="instagram" />
+      <Icon name="github" />
       <Text>Feed Header</Text>
     </Box>
   );
