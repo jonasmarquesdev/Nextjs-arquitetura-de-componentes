6fa7700479b9b9a803408da953385d90d0cd93b0
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-12-16 03:49:48 -0300

    Criando o PostService

---
+++ src/services/posts/PostsService.ts
@@ -0,0 +1,42 @@
+import fs from "fs/promises";
+import path from "path";
+import matter from "gray-matter";
+
+export interface Post {
+  metadata: {
+    date: string;
+    url: string;
+    excerpt: string;
+    tags: string[];
+  };
+  slug: string;
+  title: string;
+  content: string;
+}
+export default function PostsService() {
+  return {
+    async getAll(): Promise<Post[]> {
+      const PATH_POSTS = path.resolve(".", "_data", "posts");
+      const postFiles = await fs.readdir(PATH_POSTS, { encoding: "utf-8" });
+      const postsPromise = postFiles.map(async (postFileName) => {
+        const filePath = path.join(PATH_POSTS, postFileName);
+        const postFile = await fs.readFile(filePath, { encoding: "utf-8" });
+        const { data, content } = matter(postFile);
+        const post: Post = {
+          metadata: {
+            date: data.date.toString(),
+            url: data.url,
+            excerpt: data.excerpt,
+            tags: data.tags,
+          },
+          title: data.title,
+          slug: postFileName.replace(".md", ""),
+          content,
+        }
+        return post;
+      });
+      const posts = Promise.all(postsPromise);
+      return posts;
+    }
+  };
+}
