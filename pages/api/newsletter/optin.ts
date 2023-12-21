c84f2858fbb60386c1913ea6c4347858a662ee52
Jonas Marques <jonas.marques2021.jm5@gmail.com>
2023-12-21 11:03:33 -0300

    Utilizando database Supabase

--- pages/api/newsletter/optin.ts
+++ pages/api/newsletter/optin.ts
@@ -1,4 +1,12 @@
+import { createClient } from "@supabase/supabase-js";
 import { NextApiRequest, NextApiResponse } from "next";
+
+// Supabase Setup
+// ============
+const SUPABASE_URL = process.env.SUPABASE_URL;
+const SUPABASE_KEY = process.env.SUPABASE_KEY;
+const dbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
+// ============
 
 const httpStatus = {
   Success: 200,
@@ -8,15 +16,19 @@ const httpStatus = {
 }
 
 const controllerByMethod = {
-  POST(request: NextApiRequest, response: NextApiResponse) {
+  async POST(request: NextApiRequest, response: NextApiResponse) {
     response
       .status(httpStatus.Success)
       .json({ message: "Post request!" })
   },
-  GET(request: NextApiRequest, response: NextApiResponse) {
+  async GET(request: NextApiRequest, response: NextApiResponse) {
+    const { data, error } = await dbClient
+        .from("newsletter_users")
+        .select("*");
+
     response
       .status(httpStatus.Success)
-      .json({ message: "Get request!" })
+      .json({ message: "Get request!", total: data.length })
   }
 }
 
